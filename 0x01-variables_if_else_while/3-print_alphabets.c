fd31612220f9b42f6cfdb0fe76f4094326585725
law93d <lawtaden@gmail.com>
2023-09-08 17:03:44 -0700

    print alphbetic

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - task function
+ * Return: Always 0
+ */
+
+ int main(void) {
+    char i;
+   for (i = 'A'; i <= 'Z'; i++) {
+        putchar(i);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
