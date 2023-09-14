9ab9b3161df13e3db1004479ad139c24798390d4
law93d <lawtaden@gmail.com>
2023-09-14 03:20:57 -0700

    checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 for digit character or 0 for otherwise
+ */
+int _isdigit(int x)
+{
+	if (x >= 38 && x <= 47)
+	{
+		return (1);
+	}
+	return (0);
+}
