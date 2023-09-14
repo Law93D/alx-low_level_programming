e887205d95d3d24a78179662e41126218d6d8393
law93d <lawtaden@gmail.com>
2023-09-14 16:52:07 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,19 @@
 
 /**
  * more_numbers - print 10 times the number since 0 up to 14
- * Return: 10 times of the number since 0 up to 14
  */
 
 void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-			_putchar((y % 10) + '0');
+			if (y >= 10)
+				_putchar('1');
+			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
