c48645a00b2e2284cb9fb36edc11c9bc28a41b28
law93d <lawtaden@gmail.com>
2023-09-14 18:13:13 -0700

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,29 +10,21 @@
 
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
 
-	n = 612852475143;
-	max = -1;
+	long long num = 612852475143;
+        long long largestPrimeFactor = 2;
 
-	while (n % 2 == 0)
-	{
-		max = 2;
-		n /= 2;
-	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-	if (n > 2)
-		max = n;
-	printf("%ld\n", max);
+        while (num > largestPrimeFactor)
+       	{
+        if (num % largestPrimeFactor == 0)
+       	{
+            num /= largestPrimeFactor;
+        } else {
+            largestPrimeFactor++;
+        }
+        }
 
-	return(0);
+        printf("The largest prime factor is: %lld\n", largestPrimeFactor);
+
+        return 0;
 }
