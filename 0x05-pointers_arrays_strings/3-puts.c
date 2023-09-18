c46ee3f32a6f3bb79e7a3aa42a00dbf914b719b1
law93d <lawtaden@gmail.com>
2023-09-18 15:04:49 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - a function that prints a string
- * 
  * @str: string to print
+ * Return: void
  */
 
 void _puts(char *str)
 {
-	for (*str != '\0'; str++)
+	for (; *str != '\0'; str++)
 	{
 	       	_putchar(*str);
 	}
