536164196c95665c97259454bec4b75298bc9fc1
law93d <lawtaden@gmail.com>
2023-09-22 00:05:26 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,10 +22,7 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		{
-			j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		}
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
