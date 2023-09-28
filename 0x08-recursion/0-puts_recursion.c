96ca786072200991a5c1cd8b8e52e33dd2f2971a
law93d <lawtaden@gmail.com>
2023-09-28 13:10:09 -0700

    deep insed herself

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * puts_recursion - a function that prints a string,
- * followed by a new line
+ * _puts_recursion -  function that prints a string, followed by a new line
  * @s: string to print
  * Return: 0
  */
@@ -12,11 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	if (*s == '\0')
-	{
 		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
