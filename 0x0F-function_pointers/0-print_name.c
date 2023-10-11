b9752e7a6bdfa740b44752c03f4a320e19b8f245
law93d <lawtaden@gmail.com>
2023-10-11 14:03:44 -0700

    my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_name - a function that prints a name
+ * @name: input string
+ * @f: input pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
