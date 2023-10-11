884c5322eb38dd007e8498908535454736a339f5
law93d <lawtaden@gmail.com>
2023-10-11 14:38:05 -0700

    opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *  int_index - function that searches for an integer
+ *  @size: input number of elements in the array
+ *  @cmp: input pointer to compare values
+ *  @array: input array
+ *  Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
