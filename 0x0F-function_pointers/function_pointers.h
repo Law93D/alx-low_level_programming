70d1cdd70a73fc4de0d804e360e34466f907a08a
law93d <lawtaden@gmail.com>
2023-10-11 16:03:54 -0700

    a goal

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,6 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 int (*get_op_func(char *s))(int, int);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif
