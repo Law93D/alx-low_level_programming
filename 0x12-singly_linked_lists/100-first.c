73c7200eb39b95f65bf436ad8e4867e3e4ecc44f
Law93D <lawtaden@gmail.com>
2023-10-19 18:20:10 -0700

    print message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * main function of the program logic
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("l bore my house upon my back!\n");
+
+	return (0);
+}
