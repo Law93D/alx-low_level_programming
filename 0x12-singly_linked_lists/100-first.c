e8523b04bb6360206a48d43e5a75708b6cef075b
Law93D <lawtaden@gmail.com>
2023-10-19 18:55:35 -0700

    print message first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - program entry point
- * main function of the program logic
- * Return: 0
+ * print_message - function tha prints message
+ * before execution of main function
  */
 
-int main(void)
+void print_message(void)__attribute__((constructor));
+
+void print_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("l bore my house upon my back!\n");
-
-	return (0);
+	printf("I bore my house upon my back!\n");
 }
