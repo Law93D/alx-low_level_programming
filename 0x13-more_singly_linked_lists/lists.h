62f1e650af4d50a3adfd5783cb7267929fa00cb8
Law93D <lawtaden@gmail.com>
2023-10-23 13:56:04 -0700

    print list 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 /**
- * struct listint_t - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: point to the next node
  *
@@ -18,7 +18,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
