77b37f8eef83cc083b375fd1d272f93eda4aad6a
Law93D <lawtaden@gmail.com>
2023-12-21 08:36:45 -0800

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
@@ -11,13 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t *));
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
