0a573735a8dce9ba06d47065d6bb122f2ed67433
Law93D <lawtaden@gmail.com>
2024-04-09 15:05:45 -0700

    search algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,38 +3,38 @@
 #include "search_algos.h"
 
 /**
- * jump_search - Searches for a value in a sorted array of integers
- * using the Jump search algorithm
- * @array: Pointer to the first element of the array to search
- * @size: The number of elements in the array
- * @value: The value to search for
+ * jump_search - Searches for a value in a sorted array using jump search
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
  *
- * Return: Index where value is located, or -1 if value is not present
+ * Return: Index of the value if found, otherwise -1
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step = sqrt(size);
-    size_t prev = 0;
+    size_t step = sqrt(size), prev = 0, i;
 
-    if (array == NULL || size == 0)
+    if (array == NULL)
         return (-1);
 
-    while (array[prev] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-        prev += step;
-        if (prev >= size)
-            break;
-    }
-
-    printf("Value found between indexes [%lu] and [%lu]\n", prev - step, prev);
-    prev = (prev < size) ? prev : size - 1;
-
-    for (size_t i = prev - step; i <= prev; i++)
+    for (i = 0; i < size; i += step)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
+        if (array[i] >= value || i + step >= size)
+        {
+            printf("Value found between indexes [%lu] and [%lu]\n", prev, i);
+            printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+            while (prev <= i && array[prev] < value)
+            {
+                printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+                prev++;
+            }
+            if (prev < size && array[prev] == value)
+                return (prev);
+            else
+                return (-1);
+        }
+        prev = i;
     }
 
     return (-1);
